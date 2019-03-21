942d9ded82fda7a3be716ba9c42e37ef49778b55
coconutstickyrice <sunfare@gmail.com>
2019-03-21 22:18:27 +0000

    Lesson 14 Updates v2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,15 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.all.page(params[:page]).per_page(5)
   end
 
   def new
     @place = Place.new
   end
 
+
   def create
     Place.create(place_params)
-    redirect_to root_path
   end
 
   private
@@ -17,4 +17,5 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
+
 end
